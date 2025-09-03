ce2b5535a72c85c9c3f55125cc60e8eb0ae58158
Joaquín Antonio Galdame <92065354+JoaquinGaldame@users.noreply.github.com>
2025-09-03 01:48:40 -0300

    Commit About Me

---
+++ src/app/data/dataAboutMe.ts
@@ -0,0 +1,25 @@
+  export interface aboutMe {
+    id: number;
+    title: string;
+    content: string;
+  }
+  
+  export const cards: aboutMe[] = [
+    {
+      id: 1,
+      title: "Técnico Universitario en Programación",
+      content: "Egresado de la Facultad de Ciencias Exactas,Físicas y Naturales de la Univerdad Nacional de San Juan. Título Intermedio con el Objetivo de adquirir sólida formación en Lenguajes y principios de Programación y una visión general del funcionamiento de equipamiento computacional y desarrollo de software.",
+
+    },
+    {
+      id: 2,
+      title: "Desarrollador Web Full Stack",
+      content: "Desarrollé y optimicé sistemas de clientes en la estructuración de reportes y visualizaciones, e integraciones de nuevos módulos funcionales. Entre los proyectos gestionados se incluyen sistema TPV (Terminales Punto de Venta), plataforma de Monitoreo de Gestión, sistema web e-commerce y sistemas de soluciones ERP. Diseñé y optimicé páginas web para mejorar la usabilidad y estética visual en sistemas con actual uso de clientes, utilizando las tecnologías claves de la industria como JavaScript, HTML, CSS. Experiencia en el desarrollo de proyectos con Tailwind CSS y Angular, incluyendo versiones 13 y 17.",
+
+    },
+    {
+      id: 3,
+      title: "Licenciatura en Ciencias de la Computación",
+      content: "Estudiante avanzado en proceso de finalización de la carrera de Licenciatura en Ciencias de la Computación en la Facultad de Ciencias Exactas, Físicas y Naturales de la Universidad Nacional de San Juan. Formación profesional enfocada en la aplicación sistemática de conocimientos científicos y tecnológicos para la resolución de problemas en el ámbito de la computación. Capacitado para diseñar, desarrollar y mantener software, así como evaluar e implementar sistemas ya desarrollados.",
+    }
+  ];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,9 @@ import tpvwebicon from "../../public/projects/tpvweb/TPV-prototipo7.png"
 import smartire from "../../public/projects/smartirecontrol/Example-1.png"
 import pedidosinternos from "../../public/projects/pedidosinternos/carro-1.png"
 import styles from './ui/home.module.css'
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { animate, createDraggable, createScope, createSpring, eases, onScroll, stagger, svg, utils, waapi} from "animejs";
+import { cards } from "./data/dataAboutMe";
 
 export default function Home() {
   const headerRef = useRef(null);
@@ -15,6 +16,7 @@ export default function Home() {
   const projects = useRef(null);
   const contact = useRef(null);
   const footer = useRef(null);
+
 
   useEffect(() => {
     if ( !hero.current || 
@@ -83,7 +85,7 @@ export default function Home() {
       {/* HEADER */}
       <header  ref={headerRef} data-dir="left"  className={`bg-custom-secondary fixed top-0 left-0 w-full flex items-center justify-between px-10 py-4 shadow-lg z-50`} style={{ opacity: 0, transform: 'translateX(-200px)' }}>
         <div className="flex items-center gap-4">
-          <span className="text-2xl font-bold text-shadow-md">Joaquin Galdame</span>
+          <span className={`${styles.shinyText} text-2xl font-bold text-shadow-md`}>Joaquin Galdame</span>
         </div>
         <nav className="hidden md:flex gap-8 text-lg font-bold ">
           <a href="#about" className="hover:text-[var(--primary)] transition">Sobre mí</a>
@@ -100,7 +102,7 @@ export default function Home() {
               ¡Hola! Soy <span className="text-custom-accent ml-2">Joaquin</span>
             </div>
           </h1>
-          <p className="text-lg md:text-2xl font-noto-sans font-semibold max-w-2xl mb-1 hover:text-[var(--accent)]">
+          <p className={`text-lg md:text-2xl font-noto-sans font-semibold max-w-2xl mb-1 `}>
             Desarrollador Web Full Stack <br/>
             Técnico Universitario en Programación
           </p>
@@ -264,15 +266,146 @@ export default function Home() {
           </div>
         </div>
         <div>
-          <button className={`${styles.buttonAboutMe} ${styles.effectWave} px-6 py-4 cursor-pointer rounded-md`}>Conocerme más</button>
+          <p className="text-xl text-custom-light mt-4">
+            Desarrollador Web Full Stack Freelance y Técnico Universitario en Programación.&nbsp;Cuento con un dominio sólido de los fundamentos del desarrollo web y la programación, orientado hacia la entrega de software de alta calidad y la satisfacción de los requisitos funcionales de cada proyecto.
+          </p>
+          <button className={`${styles.buttonAboutMe} ${styles.effectWave} px-6 py-4 cursor-pointer rounded-md mt-8`}>Conocerme más</button>
         </div>
 
-        <p className="max-w-3xl leading-relaxed">
-          Soy un desarrollador orientado a resultados con experiencia en React, Next.js, Node.js y PostgreSQL.
-          Me encanta transformar ideas en soluciones digitales escalables, con especial atención al detalle
-          y la experiencia del usuario.
-        </p>
+        <div className="mx-auto mt-16 grid max-w-lg grid-cols-1 items-center gap-y-6 sm:mt-20 sm:gap-y-0 lg:max-w-[80vw] lg:grid-cols-3">
+          <div className={`${styles.gradientBorderLeft} rounded-3xl rounded-t-3xl z-10 p-8 bg-gradient-to-br from-custom-dark to-custom-primary sm:mx-8 sm:rounded-b-sm sm:p-10 lg:mx-0 lg:rounded-tr-sm lg:rounded-bl-3xl hover:rounded-3xl  ${styles.cardHoverLeft}`}>
+            <div className="bg-custom-secondary p-3 rounded-lg w-12 h-12 flex items-center justify-center mb-6">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-custom-light" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
+              </svg>
+            </div>
+            <h3 id="tier-hobby" className="text-3xl font-semibold text-custom-light text-shadow-md">Desarrollador Web Full Stack</h3>
+            <p className="mt-4 flex items-baseline gap-x-2">
+              <span className="text-base text-custom-secondary font-semibold">+ 2 años</span>
+            </p>
+            <p className="mt-5 text-base/6 text-custom-light opacity-90 leading-relaxed">Experiencia como Desarrollador Web Full Stack para la Empresa SoftSystems Soluciones para la Gestión de Negocios, integrando funcionalidades y módulos a diversos proyectos multi-empresas, administrando bases de datos y deploy en producción.&nbsp;Experiencia reciente en Desarrollo de software a medida para empresas como Electronica BIOS, CODILANT integrando nuevas soluciones a procesos especificos de control y gestión de datos.</p>
+            <ul role="list" className="mt-8 space-y-3 text-sm/6 text-custom-light sm:mt-10">
+              <li className="flex gap-x-3">
+                <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" className="h-5 w-5 flex-none text-custom-secondary mt-0.5">
+                  <path d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" fill-rule="evenodd" />
+                </svg>
+                + 10 projects
+              </li>
+              <li className="flex gap-x-3">
+                <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" className="h-5 w-5 flex-none text-custom-secondary mt-0.5">
+                  <path d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" fill-rule="evenodd" />
+                </svg>
+                Database Management and API integration
+              </li>
+              <li className="flex gap-x-3">
+                <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" className="h-5 w-5 flex-none text-custom-secondary mt-0.5">
+                  <path d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" fill-rule="evenodd" />
+                </svg>
+                Advanced analytics
+              </li>
+              <li className="flex gap-x-3">
+                <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" className="h-5 w-5 flex-none text-custom-secondary mt-0.5">
+                  <path d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" fill-rule="evenodd" />
+                </svg>
+                Deploys and server administration
+              </li>
+            </ul>
+          </div>
 
+          <div className={`${styles.gradientBorderMiddle} relative rounded-3xl z-50 p-8 bg-gradient-to-br from-custom-dark to-custom-primary to-custom-primary sm:p-10 ${styles.cardHoverPrincipal}`}>
+            <div className="bg-custom-secondary p-3 rounded-lg h-12 w-12 flex items-center justify-center mb-6">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-custom-light" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
+              </svg>
+            </div>
+            <h3 id="tier-technician" className="text-3xl font-semibold text-custom-light text-shadow-md">Técnico Universitario en Programación</h3>
+            <p className="mt-4 flex items-baseline gap-x-2">
+              <span className="text-base text-custom-secondary font-semibold">Febrero 2017 - Febrero 2023</span>
+            </p>
+            <p className="mt-5 text-base/6 text-custom-light opacity-90 leading-relaxed">Egresado de la Facultad de Ciencias Exactas,Físicas y Naturales de la Univerdad Nacional de San Juan. Título Intermedio con el Objetivo de adquirir sólida formación en Lenguajes y principios de Programación y una visión general del funcionamiento de equipamiento computacional y desarrollo de software.&nbsp; La incumbencias abarcan poseer amplios conocimientos en estrategias de programación, en diferentes paradigmas de lenguajes y tecnologías, implementación de algoritmos. Conocimientos en Sistemas Operativos, Estructuras de Computadoras y Redes, en desarrolló de software, Base de Datos y Sistemas de Información.</p>
+            <ul role="list" className="mt-8 space-y-3 text-sm/6 text-custom-light sm:mt-10">
+              <li className="flex gap-x-3">
+                <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" className="h-5 w-5 flex-none text-custom-secondary mt-0.5">
+                  <path d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" fill-rule="evenodd" />
+                </svg>
+                Languages Paradigm
+              </li>
+              <li className="flex gap-x-3">
+                <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" className="h-5 w-5 flex-none text-custom-secondary mt-0.5">
+                  <path d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" fill-rule="evenodd" />
+                </svg>
+                Data Structure
+              </li>
+              <li className="flex gap-x-3">
+                <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" className="h-5 w-5 flex-none text-custom-secondary mt-0.5">
+                  <path d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" fill-rule="evenodd" />
+                </svg>
+                Databases
+              </li>
+              <li className="flex gap-x-3">
+                <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" className="h-5 w-5 flex-none text-custom-secondary mt-0.5">
+                  <path d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" fill-rule="evenodd" />
+                </svg>
+                Python - C - C#
+              </li>
+              <li className="flex gap-x-3">
+                <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" className="h-5 w-5 flex-none text-custom-secondary mt-0.5">
+                  <path d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" fill-rule="evenodd" />
+                </svg>
+                Networks
+              </li>
+              <li className="flex gap-x-3">
+                <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" className="h-5 w-5 flex-none text-custom-secondary mt-0.5">
+                  <path d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" fill-rule="evenodd" />
+                </svg>
+                Structure and operation of computers
+              </li>
+            </ul>
+          </div>
+
+          <div className={`${styles.gradientBorderRight} rounded-3xl rounded-t-3xl z-10 p-8 bg-gradient-to-br from-custom-dark to-custom-primary sm:mx-8 sm:rounded-b-sm sm:p-10 lg:mx-0 lg:rounded-tl-sm lg:rounded-br-3xl hover:rounded-3xl  ${styles.cardHoverRight}`}>
+            <div className="bg-custom-secondary p-3 rounded-lg w-12 h-12 flex items-center justify-center mb-6">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-custom-light" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
+              </svg>
+            </div>
+            <h3 id="tier-hobby" className="text-3xl font-semibold text-custom-light">Licenciatura en Ciencias de la Computación</h3>
+            <p className="mt-4 flex items-baseline gap-x-2">
+              <span className="text-base text-custom-secondary font-semibold">Estudiante de Quinto Año - En proceso</span>
+            </p>
+            <p className="mt-5 text-base/6 text-custom-light opacity-90 leading-relaxed">Estudiante avanzado en proceso de finalización de la carrera de Licenciatura en Ciencias de la Computación en la Facultad de Ciencias Exactas, Físicas y Naturales de la Universidad Nacional de San Juan.&nbsp;Formación profesional enfocada en la aplicación sistemática de conocimientos científicos y tecnológicos para la resolución de problemas en el ámbito de la computación. Capacitado para diseñar, desarrollar y mantener software, así como evaluar e implementar sistemas ya desarrollados.</p>
+            <ul role="list" className="mt-8 space-y-3 text-sm/6 text-custom-light sm:mt-10">
+              <li className="flex gap-x-3">
+                <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" className="h-5 w-5 flex-none text-custom-secondary mt-0.5">
+                  <path d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" fill-rule="evenodd" />
+                </svg>
+                Software engineering
+              </li>
+              <li className="flex gap-x-3">
+                <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" className="h-5 w-5 flex-none text-custom-secondary mt-0.5">
+                  <path d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" fill-rule="evenodd" />
+                </svg>
+                Information theory and Cryptography
+              </li>
+              <li className="flex gap-x-3">
+                <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" className="h-5 w-5 flex-none text-custom-secondary mt-0.5">
+                  <path d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" fill-rule="evenodd" />
+                </svg>
+                IA
+              </li>
+              <li className="flex gap-x-3">
+                <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" className="h-5 w-5 flex-none text-custom-secondary mt-0.5">
+                  <path d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" fill-rule="evenodd" />
+                </svg>
+                Requirements Engineering
+              </li>
+            </ul>
+          </div>
+        </div>
       </section>
 
 
