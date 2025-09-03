13b47138ade671d90bf830e398a0f4af30d3df26
Joaquín Antonio Galdame <92065354+JoaquinGaldame@users.noreply.github.com>
2025-09-03 17:59:06 -0300

    Config to build

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export", 
   webpack(config) {
     // Primero buscá la regla que maneja imágenes de Next
     const fileLoaderRule = config.module.rules.find((rule: any) =>
